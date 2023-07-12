f94490028af70fd4b916a7f8b123db35d656fabf
van1ove <berezko2004@gmail.com>
2023-07-12 17:56:45 +0300

    game scene added

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField] private float minX, minY, maxX, maxY;
+    private void Start()
+    {
+        Vector2 pos = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        
+        PhotonNetwork.Instantiate(player.name, pos, Quaternion.identity);
+    }
+}
